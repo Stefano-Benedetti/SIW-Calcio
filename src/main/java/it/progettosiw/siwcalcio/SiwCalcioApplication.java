1c14721b5f97ab0be6186b96c6f3b274f6583512
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-06-23 18:13:23 +0200

    Initial commit

---
+++ src/main/java/it/progettosiw/siwcalcio/SiwCalcioApplication.java
@@ -0,0 +1,13 @@
+package it.progettosiw.siwcalcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwCalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwCalcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/progettosiw/siwcalcio/SiwCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.progettosiw.siwcalcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwCalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
