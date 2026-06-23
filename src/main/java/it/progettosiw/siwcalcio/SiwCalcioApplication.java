2091f3ae4d11540a08ab9a80b443f8e3a88c9062
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-06-23 18:38:10 +0200

    configurazione iniziale

--- src/main/java/it/progettosiw/siwcalcio/SiwCalcioApplication.java
+++ src/main/java/it/progettosiw/siwcalcio/SiwCalcioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiwCalcioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SiwCalcioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SiwCalcioApplication.class, args);
+    }
 
 }
