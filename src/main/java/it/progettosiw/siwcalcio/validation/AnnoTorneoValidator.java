b8cb55a7a095fc99e1f8f15da5ee0554a6e689c7
Stefano <158204304+Stefano-Benedetti@users.noreply.github.com>
2026-07-08 19:43:36 +0200

    commento modificato

--- src/main/java/it/progettosiw/siwcalcio/validation/AnnoTorneoValidator.java
+++ src/main/java/it/progettosiw/siwcalcio/validation/AnnoTorneoValidator.java
@@ -18,7 +18,7 @@ public class AnnoTorneoValidator implements Validator {
 
         int annoCorrente = LocalDate.now().getYear();
 
-        // si può modificare anche un torneo vecchio, la data minima non può essere (anno corrente - 1)
+        // si può modificare anche un torneo vecchio, quindi la data minima non può essere (anno corrente - 1)
         // 2025 perché è (anno di creazione del sistema - 1)
         if (anno < 2025 || anno > annoCorrente + 1)
             errors.rejectValue("anno", "torneo.annoNonValido");
